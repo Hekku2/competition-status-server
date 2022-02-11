6676017e939b16299a1f3358571e1d21cd77a4a7
Heikki-Jussi Niemi <hekku2@gmail.com>
2022-02-11 19:13:12 +0200

    Add missing documentation

--- src/Api.Models/PerformanceResultsContentModel.cs
+++ src/Api.Models/PerformanceResultsContentModel.cs
@@ -2,14 +2,34 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Api.Models;
 
+/// <summary>
+/// Describes a result for competitor(s) in for a single performance and what
+/// place it did achieve, if any.
+/// </summary>
 public class PerformanceResultsContentModel
 {
+    /// <summary>
+    /// Name of the division. Example: Senior women
+    /// </summary>
     [Required]
     public string Division { get; set; } = null!;
-    [Required]
-    public int CurrentPlace { get; set; }
+
+    /// <summary>
+    /// Placement that the competitor(s) received with this result. This is null
+    /// If current place couldn't be calculated.
+    /// </summary>
+    public int? CurrentPlace { get; set; }
+
+    /// <summary>
+    /// Competitors that did the performance. This should contain at least one
+    /// item.
+    /// </summary>
     [Required]
     public CompetitorModel[] Competitors { get; set; } = null!;
+
+    /// <summary>
+    /// Result/score of the performance.
+    /// </summary>
     [Required]
     public PoleSportResultModel Result { get; set; } = null!;
 }
--- src/Api/Util/CompetitionOrderUtil.cs
+++ src/Api/Util/CompetitionOrderUtil.cs
@@ -6,12 +6,9 @@ namespace Api.Util;
 
 public static class CompetitionOrderUtil
 {
-
-    public static int CalculatePlacement(CompetitionOrderEntity[] currentResults, int id)
+    public static int? CalculatePlacement(CompetitionOrderEntity[] currentResults, int id)
     {
         var order = CalculateOrder(currentResults);
-        // TODO Make this safer, test this, utilise this in competition controller mapping etc. This should also be refactored etc.
-
         for (var i = 0; i < order.Length; i++)
         {
             if (order[i].Id == id)
@@ -19,7 +16,7 @@ public static class CompetitionOrderUtil
                 return i + 1;
             }
         }
-        throw new ArgumentException($"No result found for ID #{id}");
+        return null;
     }
 
     public static CompetitionOrderEntity[] CalculateOrder(CompetitionOrderEntity[] currentResults)
