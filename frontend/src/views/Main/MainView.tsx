b644d9019ce6e3caaeae3c4d37ba97103bed2f50
Heikki-Jussi Niemi <hekku2@gmail.com>
2022-02-13 14:50:22 +0200

    Add ugly card for upcoming

--- frontend/src/views/Main/MainView.tsx
+++ frontend/src/views/Main/MainView.tsx
@@ -1,4 +1,4 @@
-import { Alert, CircularProgress } from "@mui/material";
+import { Alert, Card, CardContent, CardHeader, CircularProgress, List, ListItem, Typography } from "@mui/material";
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../components/hooks";
 import { fetchCompetitionStatus } from "../../store/competition/competitionSlice";
@@ -20,6 +20,17 @@ export const MainView = () => {
         :
         <>
           {state.competitionStatus?.currentCompetitor && <CompetitorCard header={"Current"} competitor={state.competitionStatus?.currentCompetitor} />}
+
+          {state.competitionStatus?.divisions.map(division => <Card key={division.name}>
+            <CardHeader title="Upcoming" subheader={division.name}></CardHeader>
+            <CardContent>
+              <List>
+                {division.upcomingCompetitorModels.map(item => <ListItem key={item.id}>
+                  {item.competitors.map(com => <Typography key={com.name} variant="body1">{com.name} {com.team}</Typography>)}
+                </ListItem>)}
+              </List>
+            </CardContent>
+          </Card>)}
         </>}
     </>
   );
